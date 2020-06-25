bbed6480a055a17930dc08a8e47c2f9d0c925c42
Raul Vagmacker <raulvagmacker17@gmail.com>
2020-06-25 20:18:09 -0300

    Finished

--- src/App.js
+++ src/App.js
@@ -1,30 +1,110 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { FiThumbsUp } from 'react-icons/fi';
+import Api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    Api.get('repositories').then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  //useState retorna um array com 2 posições
+  //
+  //1. Variável com o seu valor inicial
+  //2. Função para atualizarmos esse valor
+
   async function handleAddRepository() {
-    // TODO
+    const response = await Api.post('repositories', {
+      title: `Challenge React - ${Date.now()}`,
+      url: 'https://github.com/iVagmacker',
+      techs: ['NodeJS', 'ReactJs', 'React Native'],
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleLikeRepository(id) {
+    const response = await Api.post(`repositories/${id}/like`);
+
+    if (response.status === 200) {
+      const repositoryIndex = repositories.findIndex(
+        (repository) => repository.id === id
+      );
+
+      const listRepositories = [...repositories];
+
+      listRepositories[repositoryIndex].likes = response.data.likes;
+
+      setRepositories(listRepositories);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const index = repositories.findIndex((repository) => repository.id === id);
+
+    if (index >= 0) {
+      repositories.splice(index, 1);
+
+      setRepositories([...repositories]);
+
+      await Api.delete(`repositories/${id}`);
+    }
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    <>
+      <div className="board">
+        <div className="boardTitle">
+          <p>List of repositories</p>
+          <button onClick={handleAddRepository}>Adicionar</button>
+        </div>
+        <div className="cards" data-testid="repository-list">
+          {repositories.map((repository) => (
+            <div className="card" key={repository.id}>
+              <div className="title">
+                <p>{repository.title}</p>
+                <div>
+                  <FiThumbsUp />
+                  <p>{repository.likes}</p>
+                </div>
+              </div>
+              <p className="link">
+                <span>GitHub: </span>
+                <a href={repository.url} target="_blank">
+                  {repository.url}
+                </a>
+              </p>
+              <div className="techs">
+                {repository.techs.map((tech) => (
+                  <div key={tech}>{tech}</div>
+                ))}
+              </div>
+              <div className="buttons">
+                <button
+                  className="buttonLike"
+                  onClick={() => handleLikeRepository(repository.id)}
+                >
+                  Gostei
+                </button>
+                <button
+                  className="buttonTrash"
+                  onClick={() => handleRemoveRepository(repository.id)}
+                >
+                  Remover
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
